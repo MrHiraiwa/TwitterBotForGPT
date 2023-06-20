4eb2cd9c969a6ab0ec877a0d78cbd4d0786e7e3d
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-20 15:21:26 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -7,11 +7,15 @@ llm = ChatOpenAI(model="gpt-4-0613")
 
 google_search = GoogleSearchAPIWrapper()
 
+def link_results(query):
+    return google_search.results(query,10)
+
 
 tools = [
     Tool(
         name = "Search",
         func=google_search.run,
+        func=link_results,
         description="useful for when you need to answer questions about current events. it is single-input tool Search."
     ),
 ]
