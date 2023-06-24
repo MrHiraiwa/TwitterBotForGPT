84e9e2c29ad78af52ba77922d68079e5f137cdaf
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-24 18:02:56 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -18,6 +18,9 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
+# ユーザーエージェントを偽装する
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
 driver = webdriver.Chrome(options=options)  # これが正しい初期化方法です
 
 google_search = GoogleSearchAPIWrapper()
