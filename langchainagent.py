bb2e744cfe6f93dbea901a335134e6a004541e12
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-24 18:04:11 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -19,7 +19,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 # ユーザーエージェントを偽装する
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 
 driver = webdriver.Chrome(options=options)  # これが正しい初期化方法です
 
