b9387805f34eae17c85e315da56ca1edca6430b2
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-24 16:47:36 +0900

    Update langchainagent.py

--- langchainagent.py
+++ langchainagent.py
@@ -11,8 +11,20 @@ from urllib.parse import urljoin
 import openai
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+
+options = Options()
+options.add_argument("--headless") 
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install()), options=options)
 
 google_search = GoogleSearchAPIWrapper()
 
